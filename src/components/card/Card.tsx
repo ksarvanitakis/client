7af72b7b8ccb7016d65b7ea69af06024f4b14170
SumanaSattar <sumana.sattar@gmail.com>
2022-03-30 17:00:03 +0200

    routes parameters

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,14 +1,18 @@
+import { Link } from 'react-router-dom'
+
 interface CardProps {
   chef: Chef
 }
 
 function Card({ chef } : CardProps) {
-  console.log(chef.image)
   return (
     <div className="card--container">
+      <Link to={`/chefs/${chef.name}`}>
       <p>{chef.name}</p>
       <p>{chef.description}</p>
-      <img src={chef.image[1]}/>
+      <img src={chef.image[0]}/>
+      </Link>
+      
     </div>
   );
 }
---
+++ src/pages/chefProfile/ChefProfile.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useAppSelector } from '../../features/hooks'
+function ChefProfile() {
+    const chefs = useAppSelector(state=>state.chefs)
+    console.log(chefs)
+    const {name}  = useParams();
+    const chef  = chefs.find(element=>  element.name === name) as Chef
+    
+   console.log(chef)
+    return ( 
+        <>
+          <h1>Chef's Profile{chef.name}</h1>
+        </>
+     );
+}
+
+export default ChefProfile;
