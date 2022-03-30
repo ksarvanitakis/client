be009cc9ad2fb2cfe0e28ecdedb961b0910e75ed
ksarvanitakis <ksarvanitakis@hotmail.com>
2022-03-30 11:21:33 +0200

    styling the landing page

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,13 +1,23 @@
-import './hero.sass'
+import './hero.scss';
+import heroImage from './../../assets/heroImage.png';
 import { Link } from 'react-router-dom';
 function Hero() {
-    return ( 
+    return (
         <>
-        <h1>Your Kitchen, Our Chef</h1>
-        <p>We bring professional chefs to your home to prepare delicious, customized meals at a fraction of the cost.</p>
-       <button><Link to='/chefs'>Chefs</Link></button>
+            <main className='hero'>
+                <div className='hero_text-container'>
+                    <div className='hero_text-container_bg-box'></div>
+                    <h1 className='hero_text-container_title'>Your Kitchen, Our Chef</h1>
+                    <p className='hero_text-container_desc'>We bring professional chefs to your home to prepare delicious, customized meals at a fraction of the cost.</p>
+                    <button><Link to='/chefs'>Chefs</Link></button>
+                </div>
+                <div className='hero_image-container'>
+                    <div className='hero_image-container_bg-box'></div>
+                 <img className='hero_image-container_image' src={heroImage} alt='Hero'/>
+                </div>
+            </main>
         </>
-     );
+    );
 }
 
 export default Hero;
