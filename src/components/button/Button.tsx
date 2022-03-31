baf03b08502db84a19d2415b0127686e98666808
FransJoakim <fransjlt@gmail.com>
2022-03-31 14:15:59 +0200

    btn style implemented

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 interface ButtonProperties {
+    className: string,
     btnText: string,
     handleClick: React.MouseEventHandler<HTMLDivElement>,
     bgColor: string, 
@@ -8,20 +9,23 @@ interface ButtonProperties {
     hoverColor: string,
 }
 
-export default function Button({btnText, handleClick, bgColor, txtColor, hoverColor}: ButtonProperties) {
+export default function Button({className, btnText, handleClick, bgColor, txtColor, hoverColor}: ButtonProperties) {
     const [hover, setHover] = useState(false);
 
     const buttonStyle: React.CSSProperties = {
+        display: 'inline-block',
+        padding: '5px 12px',
+        margin: '1rem 0',
+        color: `${txtColor}`,
         backgroundColor: `${hover ? hoverColor : bgColor}`,
         borderRadius: '6px',
         border: '1px solid',
-        borderColor: 'black',
-        color: `${txtColor}`,
-        padding: '8px 16px'
+        borderColor: `${txtColor}`,
     } as unknown as React.CSSProperties;
 
     return (
         <div
+            className={className}
             style={buttonStyle}
             onPointerOver={() => setHover(true)}
             onPointerOut={() => setHover(false)}
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -16,7 +16,13 @@ function Hero() {
                     <div className='hero_text-container_bg-box'></div>
                     <h1 className='hero_text-container_title'>Your Kitchen, Our Chef</h1>
                     <p className='hero_text-container_desc'>We bring professional chefs to your home to prepare delicious, customized meals at a fraction of the cost.</p>
-                    <Button btnText="Booking" handleClick={routeChange} bgColor='#f9fcf2' hoverColor='#dbeeb7' txtColor='gray' />
+                    <Button 
+                        className="Hero-Btn"
+                        btnText="Booking" 
+                        handleClick={routeChange} 
+                        bgColor='#f9fcf2' 
+                        hoverColor='#dbeeb7' 
+                        txtColor='#6B7755' />
                 </div>
                 <div className='hero_image-container'>
                     <div className='hero_image-container_bg-box'></div>
