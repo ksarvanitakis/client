562926c6a557b21666c2e9dc1017c547d2b848c8
SumanaSattar <sumana.sattar@gmail.com>
2022-04-01 12:06:03 +0200

    chef page css  fixed

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom'
-
+import './card.scss'
 interface CardProps {
   chef: Chef
 }
 
-function Card({ chef } : CardProps) {
+function Card({ chef }: CardProps) {
   return (
-    <div className="card--container">
+    <div className="card-container">
       <Link to={`/chefs/${chef.name}`}>
-      <p>{chef.name}</p>
-      <p>{chef.description}</p>
-      <img src={chef.profileImage} alt='profile' />
+        <div className='card-container__profile'>
+          <div className='card-container__profile__bg-box'></div>
+          <img className='card-container__profile__img' src={chef.profileImage} alt='profile' />
+        </div>
+        <p className='card-container__chef-name'>{chef.name}</p>
+        <div className='card-container__text'>
+          <p className='card-container__text__cusine'>{chef.cusine}</p>
+          <p className='card-container__text__chef-desc'>{chef.description}</p>
+          <p className='card-container__text__price'>200 SEK/hr</p>
+        </div>
       </Link>
-      
+
     </div>
   );
 }
