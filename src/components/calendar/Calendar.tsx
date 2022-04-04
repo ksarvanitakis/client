1ab4174d896dc6ad9b208b141641d88216998add
ksarvanitakis <ksarvanitakis@hotmail.com>
2022-04-04 11:16:08 +0200

    calendar functionalities working out

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -3,23 +3,52 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 
+// const unavailableDates = ["", "2022-04-07", "2022-04-20"];
 
-const Calendar = () => {
-  const [startDate, setStartDate] = useState(new Date());
+
+
+// const Calendar = () => {
+//   const [startDate, setStartDate] = useState(new Date());
+//   const [endDate, setEndDate] = useState(null);
+//   const onChange = (dates) => {
+//     const [start, end] = dates;
+//     setStartDate(start);
+//     setEndDate(end);
+//   };
+//   return (
+//     <DatePicker
+//       selected={startDate}
+//       onChange={onChange}
+//       startDate={startDate}
+//       endDate={endDate}
+//       selectsRange
+//       inline
+//     />
+//   );
+  
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [bookedDates, setBookedDates] = useState([])
+
+  const bookedDates = (date) => {
+    const exclDates = date !== new Date().toString() //(2*24*60*60*1000)
+    console.log(exclDates)
+    return ( exclDates)
+  }
+
   return (
-    <DatePicker selected={startDate}
-      onChange={(date: Date) => setStartDate(date)}
+    <DatePicker selected={selectedDate}
+      filterDate = {bookedDates}
+      onChange={(date) => setSelectedDate(date)}
       minDate={new Date()}
-      isClearable
+      // maxDate={}
+      // isClearable
       showYearDropdown
       scrollableMonthYearDropdown
-      showTimeSelect
-      selectsStart
-      selectsEnd
+      // showTimeSelect
+      // selectsStart
+      // selectsEnd
       dateFormat="yyyy MM dd"
       inline
     />
-  );
-};
 
 export default Calendar;
