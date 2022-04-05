4c1784b5bcbcd58b663adbaceec14c26e1562ada
Barbara Kogus <barbarakogus@gmail.com>
2022-04-05 13:18:32 +0200

    rename the const

--- src/pages/checkoutPage/checkoutPage.tsx
+++ src/pages/checkoutPage/checkoutPage.tsx
@@ -6,48 +6,51 @@ function CheckoutPage() {
     const cart = useAppSelector(state => state.cart);
 
     console.log('check page', cart)
-    const shoppingList:Ingre[] = []
-    cart.dishes.forEach(el=>{
-       el.ingredients.forEach(e=>{
-        shoppingList.push({name:e.name,quantity :e.quantity*el.serving,unit:e.unit})
-       })
+    const shoppingList: Ingre[] = []
+    cart.dishes.forEach(el => {
+        el.ingredients.forEach(e => {
+            shoppingList.push({ name: e.name, quantity: e.quantity * el.serving, unit: e.unit })
+        })
     })
 
-    const dishes = shoppingList.reduce((res: Ingre[], value) => {
-        const index = res.findIndex(v => v.name === value.name)
+    const ingredientsSummary = shoppingList.reduce((ingredients: Ingre[], currentIngre) => {
+        const index = ingredients.findIndex(v => v.name === currentIngre.name)
         if (index === -1) {
-            res.push(value)
+            ingredients.push(currentIngre)
         } else {
-            const newIngre = {...value, quantity: res[index].quantity + value.quantity }
-            res[index] = newIngre;
+            const newIngre = {
+                ...currentIngre,
+                quantity: ingredients[index].quantity + currentIngre.quantity
+            }
+            ingredients[index] = newIngre;
         }
-        return res;
-      }, []);
-    
-    console.log('list', dishes)
+        return ingredients;
+    }, []);
+
+    console.log('list', ingredientsSummary)
     console.log(cart.dishes)
     console.log(shoppingList)
     let barbra;
 
     // let result = shoppingList.filter(o1 => shoppingList.some(o2 => o1.name !== o2.name));
-//    shoppingList.forEach(el=>{
-//        console.log(el.name,'i m el')
-//       barbra= shoppingList.(e=>{
-//           console.log(e.name,'i am e')
-//        return e.name  === el.name
-//       } )
-//    })
-   const common = shoppingList.filter((el,index,arry)=> arry.indexOf(el) !== index )
-   
-    return ( 
+    //    shoppingList.forEach(el=>{
+    //        console.log(el.name,'i m el')
+    //       barbra= shoppingList.(e=>{
+    //           console.log(e.name,'i am e')
+    //        return e.name  === el.name
+    //       } )
+    //    })
+    const common = shoppingList.filter((el, index, arry) => arry.indexOf(el) !== index)
+
+    return (
         <>
-         <h1>Shopping List</h1>
-        {shoppingList.map((item,index)=>{
-               return <p key={index}>{item.name} {item.quantity} {item.unit}</p>
-        })}
+            <h1>Shopping List</h1>
+            {shoppingList.map((item, index) => {
+                return <p key={index}>{item.name} {item.quantity} {item.unit}</p>
+            })}
         </>
-       
-     );
+
+    );
 }
 
 export default CheckoutPage;
