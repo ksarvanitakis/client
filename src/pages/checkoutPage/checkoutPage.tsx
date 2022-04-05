ebaa75e488a8d29bbd2e3b2c36bc601ecc637a3f
SumanaSattar <sumana.sattar@gmail.com>
2022-04-05 12:16:47 +0200

    fixing duplicated values

--- src/pages/checkoutPage/checkoutPage.tsx
+++ src/pages/checkoutPage/checkoutPage.tsx
@@ -5,9 +5,34 @@ function CheckoutPage() {
     const cart = useAppSelector(state => state.cart);
 
     console.log('check page', cart)
-  
+    const shoppingList:Ingre[] = []
+    cart.dishes.forEach(el=>{
+       el.ingredients.forEach(e=>{
+        shoppingList.push({name:e.name,quantity :e.quantity*el.serving,unit:e.unit})
+       })
+    })
+   console.log(cart.dishes)
+    console.log(shoppingList)
+    let barbra;
+
+    // let result = shoppingList.filter(o1 => shoppingList.some(o2 => o1.name !== o2.name));
+//    shoppingList.forEach(el=>{
+//        console.log(el.name,'i m el')
+//       barbra= shoppingList.(e=>{
+//           console.log(e.name,'i am e')
+//        return e.name  === el.name
+//       } )
+//    })
+   const common = shoppingList.filter((el,index,arry)=> arry.indexOf(el) !== index )
+   
     return ( 
-        <h1>Here you can checkout!</h1>
+        <>
+         <h1>Shopping List</h1>
+        {shoppingList.map((item,index)=>{
+               return <p key={index}>{item.name} {item.quantity} {item.unit}</p>
+        })}
+        </>
+       
      );
 }
 
