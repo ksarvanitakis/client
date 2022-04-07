cb06568ed5ab16ba1f452542bd356f3aacb4b270
SumanaSattar <sumana.sattar@gmail.com>
2022-04-07 11:50:43 +0200

    button working

--- src/pages/checkoutPage/checkoutPage.tsx
+++ src/pages/checkoutPage/checkoutPage.tsx
@@ -1,16 +1,19 @@
 import CheckoutButton from '../../components/checkoutButton/CheckoutButton';
 import { useAppSelector } from '../../features/hooks';
 import { updateShoppingList } from '../../features/cart/cartSlice';
-import {changeModalShowState} from '../../features/modal/modalSlice';
+import { changeModalShowState } from '../../features/modal/modalSlice';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import StepsBoard from '../../components/stepsBoard/StepsBoard';
 import OrderCard from '../../components/orderCard/orderCard';
 import HeroSub from '../../components/header/HeroSub';
-import Button from '../../components/button/Button'
+import Button from '../../components/button/Button';
+import './checkoutPage.scss'
 function CheckoutPage() {
     const cart = useAppSelector(state => state.cart);
     const dispatch = useDispatch();
+    const[displaySn,setDisplaySn]=useState(false)
+    const[display,setDisplay]=useState(true)
 
     const shoppingList: Ingre[] = []
     let ingredientsSummary: Ingre[] = [];
@@ -34,13 +37,13 @@ function CheckoutPage() {
         })
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         cart.dishes.forEach(el => {
             el.ingredients.forEach(e => {
                 shoppingList.push({ name: e.name, quantity: e.quantity * el.serving, unit: e.unit })
             })
         })
-    
+
         ingredientsSummary = shoppingList.reduce((ingredients: Ingre[], currentIngre) => {
             const index = ingredients.findIndex(v => v.name === currentIngre.name)
             if (index === -1) {
@@ -57,42 +60,58 @@ function CheckoutPage() {
 
         convertUnits();
         dispatch(updateShoppingList(ingredientsSummary));
-    
+
     }, [])
 
-    console.log('check page', cart)    
-    
+    console.log('check page', cart)
+
     return (
         <>
             <HeroSub title={'Checkout'} />
-            <OrderCard name={cart.chef.name} time={cart.totalhours} date={cart.date} price={cart.totalPrice} shopping={cart.shoppingList} dishes={cart.dishes}/>
-            <Button
-                                        className="Btn"
-                                        btnText="Log in"
-                                        bgColor='#f9fcf2'
-                                        hoverColor='#dbeeb7'
-                                        txtColor='#6B7755'
-                                        disabled={false}
-                                        handleClick={() => {
-                                            dispatch(changeModalShowState({
-                                                show: true,
-                                                type: 'login'
-                                            }))
-                                        }} />
-            <Button
-                                        className="Btn"
-                                        btnText="Sign up"
-                                        bgColor='#f9fcf2'
-                                        hoverColor='#dbeeb7'
-                                        txtColor='#6B7755'
-                                        disabled={false}
-                                        handleClick={() => {
-                                            dispatch(changeModalShowState({
-                                                show: true,
-                                                type: 'signup'
-                                            }))
-                                        }} />
-            <CheckoutButton />
+            <OrderCard name={cart.chef.name} time={cart.totalhours} date={cart.date} price={cart.totalPrice} shopping={cart.shoppingList} dishes={cart.dishes} />
+            <div className='checkout-button-container'>
+                <div className='checkout-button-container__btn' style={{display:displaySn?'none':'block'}}>
+                    <Button
+                        className="Btn"
+                        btnText="Log in"
+                        bgColor='#f9fcf2'
+                        hoverColor='#dbeeb7'
+                        txtColor='#6B7755'
+                        disabled={false}
+                        handleClick={() => {
+                            setDisplaySn(true)
+                            setDisplay(false)
+                            dispatch(changeModalShowState({
+                                show: true,
+                                type: 'login'
+                            }))
+                        }} />
+
+                </div>
+                <div className='checkout-button-container__btn' style={{display:displaySn?'none':'block'}}>
+                    <Button
+                        className="Btn"
+                        btnText="Sign up"
+                        bgColor='#f9fcf2'
+                        hoverColor='#dbeeb7'
+                        txtColor='#6B7755'
+                        disabled={false}
+                        handleClick={() => {
+                            setDisplaySn(true)
+                            setDisplay(false)
+                            dispatch(changeModalShowState({
+                                show: true,
+                                type: 'signup'
+                            }))
+                        }} />
+                </div>
+               
+
+            </div>
+            <div className='checkout-button-container__btn checkout' style={{display:display?'none':'block'}}>
+                    <CheckoutButton />
+            </div>
+
         </>
     );
 }
