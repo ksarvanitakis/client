a4aa97523f8555bd9c59f00270a7c99c89e5cad9
FransJoakim <fransjlt@gmail.com>
2022-04-07 14:25:01 +0200

    done

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
-import BookingSummaryCard from '../../components/bookingSummaryCard/BookingSummaryCard';
+import OrderCard from '../../components/orderCard/orderCard';
+import { useAppSelector } from '../../features/hooks';
+
 const baseUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001';
 
 function ProfilePage() {
 
     const [bookings, setBookings] = useState<Booking2[]>([]);
+    const cart = useAppSelector(state => state.cart);
 
     const fetchUserBookings = async () => {
         const data = await fetch(`${baseUrl}/api/users/Mr.Bean`);
@@ -21,7 +24,8 @@ function ProfilePage() {
         <>
             <h1>Profile</h1>
             {bookings.map((booking, index) => {
-                return <BookingSummaryCard booking={booking} key={index} />
+                return <OrderCard name={cart.chef.name} time={cart.totalhours} date={cart.date} price={cart.totalPrice} shopping={cart.shoppingList} dishes={cart.dishes} />
+
             })}
         </>
     );
