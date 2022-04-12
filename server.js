e6b7edefd09c41dc7fde31f7d57e76b707698b89
Barbara Kogus <barbarakogus@gmail.com>
2022-04-12 21:11:50 +0200

    Fix variable name

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
